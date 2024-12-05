8d8cfa6bea55dded9e9d57ee5f1bd1d7e3aeed67
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-12-05 14:37:10 -0300

    style: format

--- src/lib/components/cliparoo-search-example.svelte
+++ src/lib/components/cliparoo-search-example.svelte
@@ -8,121 +8,123 @@
 		<div class="relative mb-1 flex h-12 px-2">
 			<input
 				value="window:chatgpt text:cliparoo type:text"
-        disabled
-        id="search-clipboard-example"
-        aria-hidden="true"
+				disabled
+				id="search-clipboard-example"
+				aria-hidden="true"
 				placeholder="Search clipboard"
 				class="w-full rounded-btn rounded-t-none bg-base-100 px-2 outline-none ring-2 ring-accent/70"
 			/>
 		</div>
-			<div class="relative m-2">
-				<div
-					class="btn flex h-36 w-full flex-col flex-nowrap items-start justify-between p-2 text-start"
-					><span class="badge flex w-[calc(100%-2.25rem)] max-w-fit justify-start gap-1"
-						><span class="overflow-hidden text-ellipsis whitespace-nowrap"
-							>ChatGPT - Google Chrome</span
-						>
-						<svg
-							class="h-4 w-4 flex-shrink-0"
-							fill="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-							><path
-								d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-							></path></svg
-						></span
+		<div class="relative m-2">
+			<div
+				class="btn flex h-36 w-full flex-col flex-nowrap items-start justify-between p-2 text-start"
+			>
+				<span class="badge flex w-[calc(100%-2.25rem)] max-w-fit justify-start gap-1"
+					><span class="overflow-hidden text-ellipsis whitespace-nowrap"
+						>ChatGPT - Google Chrome</span
 					>
-					<p
-						style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
-						class="overflow-hidden text-ellipsis break-all pl-1 text-sm"
-					>
-						ChatGPT, Cliparoo is amazing. Isn't it?
-					</p>
-					<div class="card-actions items-center justify-between">
-						<span class="badge badge-primary">text</span>
-						<time class="text-sm">December 5, 2024</time>
-					</div></div
+					<svg
+						class="h-4 w-4 flex-shrink-0"
+						fill="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+						><path
+							d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+						></path></svg
+					></span
 				>
-				<div class="absolute right-2 top-[calc(0.5rem+2px)] p-0">
-					<div
-						class="btn btn-sm size-8 border-none bg-transparent p-0 ring-base-content hover:border focus:ring-2"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							class="lucide lucide-ellipsis-vertical"
-							><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle
-								cx="12"
-								cy="19"
-								r="1"
-							></circle></svg
-						>
-					</div>
+				<p
+					style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
+					class="overflow-hidden text-ellipsis break-all pl-1 text-sm"
+				>
+					ChatGPT, Cliparoo is amazing. Isn't it?
+				</p>
+				<div class="card-actions items-center justify-between">
+					<span class="badge badge-primary">text</span>
+					<time class="text-sm">December 5, 2024</time>
 				</div>
 			</div>
-			<div class="relative m-2">
+			<div class="absolute right-2 top-[calc(0.5rem+2px)] p-0">
 				<div
-					class="btn flex h-36 w-full flex-col flex-nowrap items-start justify-between p-2 text-start"
-					><span class="badge flex w-[calc(100%-2.25rem)] max-w-fit justify-start gap-1"
-						><span class="overflow-hidden text-ellipsis whitespace-nowrap"
-							>ChatGPT - Google Chrome</span
-						>
-						<svg
-							class="h-4 w-4 flex-shrink-0"
-							fill="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-							><path
-								d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-							></path></svg
-						></span
-					>
-					<p
-						style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
-						class="overflow-hidden text-ellipsis break-all pl-1 text-sm"
-					>
-						It sure sounds like Cliparoo is shaping up to be an awesome tool! It seems like you've
-						been adding some great features to it, like search and filter explanations. I'm sure the
-						users will appreciate the flexibility and ease of use you're building into the app! What
-						part of Cliparoo are you most excited about right now?
-					</p>
-					<div class="card-actions items-center justify-between">
-						<span class="badge badge-primary">text</span>
-						<time class="text-sm">December 5, 2024</time>
-					</div></div
+					class="btn btn-sm size-8 border-none bg-transparent p-0 ring-base-content hover:border focus:ring-2"
 				>
-				<div class="absolute right-2 top-[calc(0.5rem+2px)] p-0">
-					<div
-						class="btn btn-sm size-8 border-none bg-transparent p-0 ring-base-content hover:border focus:ring-2"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-ellipsis-vertical"
+						><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle
+							cx="12"
+							cy="19"
+							r="1"
+						></circle></svg
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							class="lucide lucide-ellipsis-vertical"
-							><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle
-								cx="12"
-								cy="19"
-								r="1"
-							></circle></svg
-						>
-					</div>
 				</div>
 			</div>
+		</div>
+		<div class="relative m-2">
+			<div
+				class="btn flex h-36 w-full flex-col flex-nowrap items-start justify-between p-2 text-start"
+			>
+				<span class="badge flex w-[calc(100%-2.25rem)] max-w-fit justify-start gap-1"
+					><span class="overflow-hidden text-ellipsis whitespace-nowrap"
+						>ChatGPT - Google Chrome</span
+					>
+					<svg
+						class="h-4 w-4 flex-shrink-0"
+						fill="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+						><path
+							d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+						></path></svg
+					></span
+				>
+				<p
+					style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
+					class="overflow-hidden text-ellipsis break-all pl-1 text-sm"
+				>
+					It sure sounds like Cliparoo is shaping up to be an awesome tool! It seems like you've
+					been adding some great features to it, like search and filter explanations. I'm sure the
+					users will appreciate the flexibility and ease of use you're building into the app! What
+					part of Cliparoo are you most excited about right now?
+				</p>
+				<div class="card-actions items-center justify-between">
+					<span class="badge badge-primary">text</span>
+					<time class="text-sm">December 5, 2024</time>
+				</div>
+			</div>
+			<div class="absolute right-2 top-[calc(0.5rem+2px)] p-0">
+				<div
+					class="btn btn-sm size-8 border-none bg-transparent p-0 ring-base-content hover:border focus:ring-2"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-ellipsis-vertical"
+						><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle
+							cx="12"
+							cy="19"
+							r="1"
+						></circle></svg
+					>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
